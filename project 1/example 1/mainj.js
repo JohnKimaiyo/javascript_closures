1cfad41143327aae9153a6bb8b683d4ba10290eb
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-15 22:42:43 -0800

    first commit

---
+++ project 1/example 1/mainj.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+function newcar(car,department){
+    if (car === "koushik") {
+        var department = "Engineering"
+        var car = "koushik";
+    }
+    
+}
+console.log(car);
+console.log(department);
+
+newcar();
