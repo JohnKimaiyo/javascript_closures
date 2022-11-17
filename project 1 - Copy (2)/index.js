563baa82e24e4f731545a94da5ce28f2a727adbd
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-17 02:49:00 -0800

    accessing variable without keyword oustdise a function

--- project 1 - Copy (2)/index.js
+++ project 1 - Copy (2)/index.js
@@ -1,13 +1,7 @@
-var a = 10;
+a = 10;
 
-function outer() {
-    var b = 20;
-
-    function inner() {
-        console.log(a);
-        console.log(b);
-    }
-    inner();
+function myfunction(){
+    console.log(a*a);
 }
 
-outer();
+myfunction();
