f0a5d0d2e0413ee009f800c374ae5419e7e53fd9
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-17 02:45:29 -0800

    accessing variable inside a function without a keyword

--- project 1 - Copy (2)/main.js
+++ project 1 - Copy (2)/main.js
@@ -1,13 +1,6 @@
-var a = 10;
-
-function outer() {
-    var b = 20;
-
-    function inner() {
-        console.log(a);
-        console.log(b)
-    }
-    inner()
+function myfunction(){
+    a = 10;
+    console.log(a*a);
 }
 
-outer();
+myfunction();
