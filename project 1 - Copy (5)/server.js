be6f9487ce48d35e5bd6f1082d1b58f8c379dcdd
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-17 03:54:43 -0800

    accessing vaiable outside th function without keyword

---
+++ project 1 - Copy (5)/server.js
@@ -0,0 +1,19 @@
+var age = 15
+
+function myfunction(age) {
+    if (age < 18) {
+        console.log("person is a minor")
+    } else if (age >= 18) {
+        console.log("person is an adult")
+    } else {
+        console.log("outside parameters")
+    }
+}
+
+myfunction(25);
+myfunction(17);
+
+
+
+
+
