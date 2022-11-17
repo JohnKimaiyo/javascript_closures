e2524662551467aa4a613618e525a98ee13ccd4f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-17 02:43:20 -0800

    accessing variable outside a  function

--- project 1 - Copy (2)/server.js
+++ project 1 - Copy (2)/server.js
@@ -1,13 +1,6 @@
-var a = 10;
+let a = 10;
 
-function outer(){
-    var b = 20;
-
-    function inner(){
-        console.log(a);
-        console.log(b);
-    }
-    inner();
+function myfunction(){
+    console.log(a*a);
 }
-
-outer();
+myfunction();
