84dadcaf29f8deb4555ffdabdb1e7a6720a98ef1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-16 23:06:35 -0800

    variable created without keyword

--- project 1 - Copy (11)/app.js
+++ project 1 - Copy (11)/app.js
@@ -1,18 +1,8 @@
-var a = 10;
-
-function outer(){
-    var b = 10;
-
-    var inner = function(){
-
-        console.log(a);
-        console.log(b);
-    };
-
-    return inner;
+function myfunction() {
+    a = 4;
+    console.log(a * a);
 }
 
 
-var innerfn = outer();
 
-innerf
+myfunction();
