87e73960883061f88512d2b75837bdafa66d4eaf
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-17 06:47:22 -0800

    accessing variable inside a function

--- project 1 - Copy (10)/app.js
+++ project 1 - Copy (10)/app.js
@@ -1,13 +1,7 @@
-var a = 10;
-
-function outer(){
-    var b = 20;
-
-    function inner(){
-        console.log(a);
-        console.log(b);
-    }
-    inner();
+function myfunction(){
+    let a = 10;
+    console.log(a*a);
 }
 
-outer();
+myfunction();
+
